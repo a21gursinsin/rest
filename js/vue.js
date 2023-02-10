4a68b75019894cc49fee977cd5cfc3b86f178167
a21gursinsin <a21gursinsin@gmail.com>
2023-02-10 18:59:22 +0100

    añsdkjnv

--- js/vue.js
+++ js/vue.js
@@ -628,7 +628,7 @@ Vue.component("reservation", {
         "http://singh.alumnes.inspedralbes.cat/js/backend/reserva.php";
       await fetch(url, {
         method: "POST",
-        body: "Hola",
+        body: enviar,
       })
         .then((response) => response)
         .then((data) => console.log(data));
