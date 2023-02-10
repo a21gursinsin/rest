ea8792dede932b5bd11b06cc1cf08fb63af7487b
a21gursinsin <a21gursinsin@gmail.com>
2023-02-10 23:48:24 +0100

    MM

--- js/backend/reserva.php
+++ js/backend/reserva.php
@@ -59,6 +59,22 @@ if (empty($nombre) || empty($mail) || empty($dia) || empty($hora) || empty($pers
 
 if ($resultat != 'err' and $resultat != 'err2') {
 
+    //Para Restaurante
+    $to = "gur.coder@gmail.com";
+    $subject = "Reserva de" . $nombre;
+
+    $message = "<b>This is HTML message.</b>";
+    $message .= "<h1>This is headline.</h1>";
+
+    $header = "From:" . $mail . "\r\n";
+    $header .= "Cc:afgh@somedomain.com \r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html\r\n";
+
+    $retval = mail($to, $subject, $message, $header);
+
+
+    //Para Cliente
     $to = "gur.coder@gmail.com";
     $subject = "Reserva de" . $nombre;
 
