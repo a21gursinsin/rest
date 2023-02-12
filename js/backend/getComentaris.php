9783e23a41f37b2bd8c321e949c6e4efcfe99e2f
a21gursinsin <a21gursinsin@gmail.com>
2023-02-12 12:15:36 +0100

    dd

--- js/backend/getComentaris.php
+++ js/backend/getComentaris.php
@@ -1,9 +1,10 @@
 <?php
 if (file_exists('./output.json')) {
     $data = file_get_contents("./output.json");
+    $resultat[] = $data;
 } else {
     $resultat[] = 'err';
 }
 
 
-echo json_encode($data);
+echo json_encode($resultat);
